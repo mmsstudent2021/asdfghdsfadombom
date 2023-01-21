7378c879b582c9e4bf73836c4b6fead6fc1df4b0
mmsstudent2021 <mmsstudent2021@gmial.com>
2023-01-21 14:00:56 +0630

    update invoice

--- js/invoice.js
+++ js/invoice.js
@@ -29,6 +29,7 @@ const products = [
 
 //selector
 const app = document.querySelector("#app");
+const table = document.querySelector("#table");
 const builder = document.querySelector("#builder");
 const addListForm = document.querySelector("#addListForm");
 const selectService = document.querySelector("#selectService");
@@ -38,7 +39,7 @@ const subTotal = document.querySelector("#subTotal");
 const gst = document.querySelector("#gst");
 const total = document.querySelector("#total");
 
-// funciton
+// function
 const addNewList = (event) => {
   event.preventDefault();
   // id ကိုသိတာနဲ့ product လိုချင်တယ်
@@ -50,22 +51,40 @@ const addNewList = (event) => {
   //     currentProduct.price,
   //     quantity.valueAsNumber
   //   );
-  rows.append(
-    creatRow(currentProduct.title, currentProduct.price, quantity.valueAsNumber)
+
+  const isExistedProduct = [...rows.children].find(
+    (el) => el.getAttribute("product-id") == currentProduct.id
   );
+
+  console.log(isExistedProduct);
+
+  if (isExistedProduct) {
+    const rq = isExistedProduct.querySelector(".row-quantity");
+    const rt = isExistedProduct.querySelector(".row-total");
+
+    rq.innerText = parseFloat(rq.innerText) + quantity.valueAsNumber;
+    rt.innerText = rq.innerText * currentProduct.price;
+
+  } else {
+    rows.append(createRow(currentProduct, quantity.valueAsNumber));
+  }
 
   addListForm.reset();
   calculateTotal();
 };
 
-const creatRow = (title, price, quantity) => {
-  const total = price * quantity;
+const createRow = (product, quantity) => {
+  const total = product.price * quantity;
 
   const tr = document.createElement("tr");
+  tr.setAttribute("product-id", product.id);
   tr.innerHTML = `
-        <td>${title}</td>
-        <td class="text-end">${quantity}</td>
-        <td class="text-end">${price}</td>
+        <td class=" d-flex justify-content-between">
+        ${product.title}
+        <i class=" bi bi-trash3 text-danger del-btn"></i>
+        </td>
+        <td class="text-end row-quantity">${quantity}</td>
+        <td class="text-end">${product.price}</td>
         <td class="text-end row-total">${total}</td> 
     `;
   return tr;
@@ -91,6 +110,12 @@ const calculateTotal = () => {
   subTotal.innerText = calculateSubTotal;
   gst.innerText = calculateGst;
   total.innerText = calculateFinalTotal;
+
+  if(rows.children.length){
+    table.classList.remove("d-none")
+  }else{
+    table.classList.add("d-none")
+  }
 };
 
 // process
@@ -99,3 +124,19 @@ products.forEach((product) =>
 );
 
 addListForm.addEventListener("submit", addNewList);
+
+// document.querySelectorAll(".del-btn").forEach((el) => {
+//   el.addEventListener("click", (event) => {
+//     console.log(event);
+//   });
+// });
+
+app.addEventListener("click", (event) => {
+  // console.log(event.target);
+  if (event.target.classList.contains("del-btn")) {
+    if (confirm("Are U sure to delete ?")) {
+      event.target.closest("tr").remove();
+      calculateTotal();
+    }
+  }
+});
